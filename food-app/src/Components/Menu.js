c965c743c9de89ad3ff1199e0a1c10056654d6c3
Samson Ayodele <idowusamson0777@gmail.com>
2022-08-24 15:56:54 +0100

    Menu

---
+++ food-app/src/Components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className='menu'>
+        <div className='menu--text'>
+            <h1>Special Meals of the day!</h1>
+            <p>Check our specials of the day and get discounts on all our meals<br/> and swift delivery to what ever location within Ilorin.</p>
+        </div>
+        <div className='menu--description'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Menu
--- food-app/src/Page/LandingPage.js
+++ food-app/src/Page/LandingPage.js
@@ -2,6 +2,11 @@ import React from 'react'
 import Logo from '../assets/lillieslogo.svg'
 import Nav from '../Components/Nav'
 import { Link } from 'react-router-dom'
+import Soc1 from '../assets/Group.svg'
+import Soc2 from '../assets/Layer 2.svg'
+import Img1 from '../assets/head.svg'
+import Menu from '../Components/Menu'
+
 
 
 const LandingPage = () => {
@@ -24,14 +29,19 @@ const LandingPage = () => {
         </div>
 
         <div className='heading'>
-            <div>
-                <h1>Order food anytime, anywhere</h1>
-                <p>Browse from our list of specials to place your order and have food delivered to you in no time. Affordable, tasty and fast!</p>
+            <div className='heading--text'>
+                <h1>Order food anytime, <br />anywhere</h1>
+                <p>Browse from our list of specials to place your order and have food<br/> delivered to you in no time. Affordable, tasty and fast!</p>
                 <span>
-                    
+                    <img src={Soc1} alt='soc'></img>
+                    <img src={Soc2} alt='soc'></img>
                 </span>
             </div>
+            <div className='heading--image'>
+                <img src={Img1} alt='img'></img>
+            </div>
         </div>
+        <Menu />
     </div>
   )
 }
