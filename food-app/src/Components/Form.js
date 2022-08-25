f94156b2f8052ea581760a2fe6090383368b6b04
Samson Ayodele <idowusamson0777@gmail.com>
2022-08-25 14:08:08 +0100

    login

---
+++ food-app/src/Components/Form.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Form = () => {
+
+  const [inputs, setInputs] = useState({})
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs)
+  }
+
+  return (
+        <div className='form'>
+            <h1>WELCOME BACK!</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input type='text' name='email' value={inputs.email || ""} placeholder='enter your email'
+                    onChange={handleChange} 
+                    />
+                </label><br/>
+                <label>
+                    <input type='text' name='password' value={inputs.password || ""} placeholder='enter your password'
+                    onChange={handleChange} 
+                    />
+                </label> <br/>
+                <input type='submit'/>
+            </form>
+        </div>
+  )
+}
+
+export default Form
--- food-app/src/Page/LogInPage.js
+++ food-app/src/Page/LogInPage.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Image from '../assets/logImg.svg'
+import Form from '../Components/Form'
 
 const logInPage = () => {
   return (
-    <div>logInPage</div>
+    <div className='login'>
+      <div className='login--img'>
+        <img src={Image} alt='login'></img>
+      </div>
+      <div>
+        <div className='logform'>
+          <Form />
+        </div>
+        <div form--footer>
+          <div>
+            <p>Create an account</p>
+          </div>
+          <div>
+            <p>Forgot Password</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
