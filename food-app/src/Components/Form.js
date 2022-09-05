197ec956f0eefdd0a43911163859953fb290cd02
Samson Ayodele <idowusamson0777@gmail.com>
2022-09-05 14:23:45 +0100

    modified

--- food-app/src/Components/Form.js
+++ food-app/src/Components/Form.js
@@ -18,7 +18,9 @@ const Form = () => {
     let user = JSON.parse(sessionStorage.getItem('user'))
     if(inputs?.email === user?.email && inputs?.password === user?.password) {
       toast.success(`you have successfully logged in`)
-      window.location='/dashboard'
+      setInterval(() =>{
+        window.location='/dashboard'
+      }, 2000) 
     }else {
       toast.error('wrong email or password')
     }
